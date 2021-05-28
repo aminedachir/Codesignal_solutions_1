4c716f184a5c1aba6430b6d1ddc85567b6b4b95c
aminedachir <dachiramine110@gmail.com>
2021-05-28 23:21:37 +0100

    0/14

--- the core/circleofnumbers.py
+++ the core/circleofnumbers.py
@@ -1,16 +1,3 @@
 def circleOfNumbers(n, firstNumber):
-    a = n/2 + firstNumber
-    if n<=10:
-        if a<10:
-            return a
-        elif a>10:
-            a = a-10
-            return a
-    elif 15<n<20:
-        return (n/2 + firstNumber)
-    
-    elif n>10<15:
-        return n/3
-        
-    elif n==6:
-        return (n/4 - firstNumber)
+    a = n/2
+    for i in range (n):
