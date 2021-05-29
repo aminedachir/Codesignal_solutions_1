2b4f88c23afaa39e5298e05eefa4050abe5b132a
aminedachir <dachiramine110@gmail.com>
2021-05-29 23:30:36 +0100

    late ride 13/14

--- the_core/lateride.py
+++ the_core/lateride.py
@@ -1,12 +1,10 @@
-import math
-n = input("number: ")
 def lateRide(n):
     for i in range(n):
         if n==0:
-            print(n)
+            return n
         else:
             mint = math.floor(n/60)
             c = n-mint * 60
             a = mint/10 + mint%10
             b = c/10 + c%10
-            print(int(a)+int(b))
+            return int(a)+int(b)
