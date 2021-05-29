cc47cff7c557e850de442133125d2f8659cb0889
aminedachir <dachiramine110@gmail.com>
2021-05-29 23:33:16 +0100

    late ride 1/12

--- the_core/lateride.py
+++ the_core/lateride.py
@@ -1,10 +1,4 @@
 def lateRide(n):
-    for i in range(n):
-        if n==0:
-            return n
-        else:
-            mint = math.floor(n/60)
-            c = n-mint * 60
-            a = mint/10 + mint%10
-            b = c/10 + c%10
-            return int(a)+int(b)
+    h = math.floor(n/60)
+    mint = n / 60
+    return (h/ 10) + (h % 10) + (mint / 10) + (mint % 10)
